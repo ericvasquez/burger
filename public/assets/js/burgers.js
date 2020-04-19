9637f34ed4c849428a39001d1a8de2c6ad7ba68d
Eric <eric.vasquez3327@gmail.com>
2020-04-18 23:30:39 -0500

    can no longer change devoured from true

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -27,7 +27,7 @@ $(function() {
 
     var newBurger = {
       burger_name: $("#ca").val().trim(),
-      devoured: $("[name=devoured]:checked").val().trim()
+      devoured: 1
     };
 
     // Send the POST request.
